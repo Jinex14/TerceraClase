0ec224cc264a5f2550ee250fa75cda5806f03f16
Jinex velarde urbina <jeanvelarde2@gmail.com>
2017-11-06 06:24:06 -0500

    agregar Botom

--- clase3git-angular/src/app/material/material.module.ts
+++ clase3git-angular/src/app/material/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-import {MatToolbarModule, MatButtonModule} from '@angular/material';
+import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [MaterialDemoComponent]
 })
