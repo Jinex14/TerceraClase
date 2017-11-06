b61e05ad806576dfd20f1f59cb72b1d8c7d6e9e8
Jinex velarde urbina <jeanvelarde2@gmail.com>
2017-11-06 03:39:39 -0500

    creacion del proyecto

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 ------ANGULAR CLI CON MATERIAL DESING---------
 ----------------------------------------------
 
+crear la aplicacion y en consola pones lo siguiente
+npm install --save @angular/material @angular/cdk
+luego se instala las animaciones con este comando
+npm install --save @angular/animations
+
+se debe agregar esta linea en el app module 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+y se agrega al Ngmodule el BrowserAnimationsModule
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
